f53742e4d0546a0a596c0b83130d5ac3dc96d50b
chanyutl <chanyutx10@gmail.com>
2018-05-24 14:39:52 +0700

    add CountAnyCollectionWithQuery

--- mgodbquery.go
+++ mgodbquery.go
@@ -1,7 +1,18 @@
 package core
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
 // MgoDBQuery ...
 type MgoDBQuery struct {
 	Offset int `json:"offset"`
 	Limit  int `json:"limit"`
+}
+
+func CountAnyCollectionWithQuery(collectionName string, query bson.M) (int, error) {
+	db := MgoDb{}
+	db.InitByRevelConfig()
+	defer db.Close()
+	return db.C(collectionName).Find(query).Count()
 }
